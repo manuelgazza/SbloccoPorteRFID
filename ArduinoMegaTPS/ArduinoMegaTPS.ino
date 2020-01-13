c1eee891ccf77fc0ced7ee598c3c8c9de279db81
Manuel Gazzaniga <48283097+manuelgazza@users.noreply.github.com>
2020-01-13 23:11:08 +0100

    Versione 2
    Aggiunta divisione in moduli
    Aggiunto vecchio file .ino di un nostro progetto contenente prove da cui implementare setup e pinout della tastiera 4x4 a membrane

---
+++ ArduinoMegaTPS/ArduinoMegaTPS.ino
@@ -0,0 +1,15 @@
+bool receivedBinData[6];
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void getBinData() {
+  
+}
