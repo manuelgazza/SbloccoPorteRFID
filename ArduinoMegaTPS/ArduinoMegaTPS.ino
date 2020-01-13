7511d9e0ec73e2e44572bad364474f20ca9aa42b
Manuel Gazzaniga <48283097+manuelgazza@users.noreply.github.com>
2020-01-13 23:33:38 +0100

    Versione 2.1

--- ArduinoMegaTPS/ArduinoMegaTPS.ino
+++ ArduinoMegaTPS/ArduinoMegaTPS.ino
@@ -1,7 +1,26 @@
 bool receivedBinData[6];
 
+// ClockPin = 30 (receiver)
+
+#define pinPorta_1 22
+
+/*
+ * PORTE:
+ * Porta_1 = 22
+ * Porta_2 = 23
+ * Porta_3 = 24
+ * Porta_4 = 25
+ * Porta_5 = 26
+ * Porta_6 = 27
+ */
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(22, INPUT);
+  pinMode(23, INPUT);
+  pinMode(24, INPUT);
+  pinMode(25, INPUT);
+  pinMode(26, INPUT);
+  pinMode(27, INPUT);
 
 }
 
@@ -11,5 +30,10 @@ void loop() {
 }
 
 void getBinData() {
+  
+}
+
+int getRoomNum() {
+  // if clock == 1 && 22 == Low
   
 }
